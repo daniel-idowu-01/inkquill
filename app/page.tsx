9c6f8c7c0ea6b29da5128b5dbf1274d0b883374b
daniel-idowu-01 <danielidowu414@gmail.com>
2024-04-30 12:39:13 +0100

    completed summarize section (tablet) 2

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
         <Hero />
       </section>
 
-      <section className="text-center w-[90%] lg:w-[60%] mx-auto py-10 lg:py-20">
+      <section className="text-center w-[90%] lg:w-[60%] mx-auto py-10 md:pt-0 lg:pb-20">
         <p className="text-black text-2xl md:text-4xl tracking-wide mb-3">
           What is Ink
           <span className="text-azure-blue font-semibold">Quill?</span>
