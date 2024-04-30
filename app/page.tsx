0efa21b28b6f8c25e1224c624c30fd50bf16b4d7
daniel-idowu-01 <danielidowu414@gmail.com>
2024-04-30 01:35:12 +0100

    completed hero page (mobile)

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,35 @@
 import Image from "next/image";
 import NavBar from "./components/NavBar";
 import Button from "./ui/Button";
-import HeroImage from './ui/hero-image/hero-image.svg'
+import HeroImage from "./ui/hero-image/hero-image.svg";
 import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="md:bg-cotton-white h-screen">
-      <div className="bg-azure-blue md:bg-cotton-white p-7 px-10">
+    <main className="md:bg-cotton-white">
+      <div className="text-cotton-white md:text-black bg-azure-blue md:bg-cotton-white p-7 md:px-10 rounded-br-[2rem] h-svh">
         <NavBar />
 
-        <section className="flex justify-between items-center py-20 md:py-32">
-          <article className="w-[60%]">
-            <p className="kanit-semibold text-7xl uppercase leading-tight">Tap into the strength of your notes</p>
-            <p className="text-xl my-5">Work intelligently rather than simply putting in more effort</p>
-            <Button label='Try It Yourself' />
+        <section className="flex flex-col md:flex-row justify-between items-center gap-10 md:gap-0 py-24 md:py-32 text-center md:text-left">
+          <article className="md:w-[60%]">
+            <p className="kanit-semibold text-5xl md:text-7xl uppercase leading-tight">
+              Tap into the <span className="md:text-azure-blue">strength</span>{" "}
+              of your notes
+            </p>
+            <p className="text-xl my-7 md:my-5">
+              Work intelligently rather than simply putting in more effort
+            </p>
+            <Button label="Try It Yourself" />
           </article>
 
-          <article>
+          <article className="hidden md:block">
             <Image src={HeroImage} width={500} height={500} alt="" />
           </article>
         </section>
+      </div>
+
+      <div>
+        sksksk
       </div>
     </main>
   );
--- app/ui/Button.tsx
+++ app/ui/Button.tsx
@@ -6,7 +6,7 @@ interface ButtonProps {
 
 const Button: React.FC<ButtonProps> = ({ label }) => {
   return (
-    <button className="bg-azure-blue text-cotton-white px-6 py-3 rounded-md border border-azure-blue hover:bg-transparent hover:text-azure-blue transition-all font-[550]">
+    <button className="bg-cotton-white md:bg-azure-blue text-azure-blue md:text-cotton-white px-6 py-3 rounded-md border border-azure-blue hover:bg-transparent hover:text-azure-blue transition-all font-[550]">
       {label}
     </button>
   );
