6b7b562fe677367eeda5746438258b409ab0bd83
daniel-idowu-01 <danielidowu414@gmail.com>
2024-04-30 12:36:32 +0100

    completed summarize section (tablet)

--- app/components/Summarize.tsx
+++ app/components/Summarize.tsx
@@ -4,11 +4,11 @@ import Button from "../ui/Button";
 
 const Summarize = () => {
   return (
-    <section className="flex flex-col md:flex-row gap-10 md:gap-0 items-center bg-azure-blue p-10 lg:p-20 rounded-tr-3xl rounded-bl-3xl">
-      <article className="md:w-1/2">
+    <section className="flex flex-col lg:flex-row gap-10 lg:gap-0 items-center bg-azure-blue p-10 lg:p-20 rounded-tr-3xl rounded-bl-3xl">
+      <article className="lg:w-1/2">
         <Image src="/images/summarize.png" width={500} height={500} alt="" />
       </article>
-      <article className="md:w-1/2 text-cotton-white">
+      <article className="lg:w-1/2 text-cotton-white">
         <p className="text-4xl font-semibold">Summarize Notes</p>
         <p className="my-5 text-lg">
           InkQuill utilizes advanced artificial intelligence to condense your
--- app/page.tsx
+++ app/page.tsx
@@ -5,12 +5,12 @@ import Summarize from "./components/Summarize";
 export default function Home() {
   return (
     <main className="md:bg-cotton-white">
-      <section className="text-cotton-white md:text-black bg-azure-blue md:bg-cotton-white p-7 md:px-10 lg:px-14 rounded-br-[2rem] h-svh">
+      <section className="text-cotton-white md:text-black bg-azure-blue md:bg-cotton-white p-7 md:px-10 lg:px-14 rounded-br-[2rem] h-svh md:h-fit">
         <NavBar />
         <Hero />
       </section>
 
-      <section className="text-center w-[80%] lg:w-[60%] mx-auto py-10 lg:py-20">
+      <section className="text-center w-[90%] lg:w-[60%] mx-auto py-10 lg:py-20">
         <p className="text-black text-2xl md:text-4xl tracking-wide mb-3">
           What is Ink
           <span className="text-azure-blue font-semibold">Quill?</span>
