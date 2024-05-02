74d2be53044f3ae20002ba1a440b4d023cb3964f
daniel-idowu-01 <danielidowu414@gmail.com>
2024-05-03 00:10:12 +0100

    completed paraphrase section

--- app/paraphraser/page.tsx
+++ app/paraphraser/page.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Link from "next/link";
 import { NavBar } from "../components";
+import Button from "../ui/Button";
 
 const Paraphraser = () => {
   return (
@@ -19,7 +20,24 @@ const Paraphraser = () => {
       </section>
 
       {/* main content of the page */}
-      <section></section>
+      <section className="flex flex-col md:flex-row justify-center h-80 gap-10">
+        <textarea
+          name="paste-text"
+          id="paste-text"
+          cols={30}
+          rows={10}
+          placeholder="Paste text"
+          className="mx-auto md:mx-0 border-2 border-azure-blue border-dashed w-[90%] md:w-[40%] focus:outline-azure-blue p-2 rounded-md"
+        ></textarea>
+
+        <article className="mx-auto md:mx-0 bg-azure-blue text-cotton-white w-[90%] md:w-[40%] h-96 md:h-full rounded-md p-2">
+          Paraphrased text
+        </article>
+      </section>
+
+      <div className="flex justify-center mt-10 bg-azure-blue md:bg-transparent p-3 md:p-0 rounded-tr-3xl rounded-bl-3xl">
+        <Button whiteBg={false} label="Paraphrase" />
+      </div>
     </main>
   );
 };
