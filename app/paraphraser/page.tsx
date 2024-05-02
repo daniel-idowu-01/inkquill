d8f70df8a149d8d0116c81e177834f06d5e14608
daniel-idowu-01 <danielidowu414@gmail.com>
2024-05-02 22:57:48 +0100

    fix: paraphrase and summarize link styles

--- app/paraphraser/page.tsx
+++ app/paraphraser/page.tsx
@@ -4,20 +4,21 @@ import { NavBar } from "../components";
 
 const Paraphraser = () => {
   return (
-    <main className="">
+    <main>
       <section className="p-7 md:px-10 lg:px-14 bg-azure-blue md:bg-cotton-white">
         <NavBar />
       </section>
       <section className="flex items-center justify-center gap-5 py-10">
+        <Link href="/summarizer">Summarize</Link>
         <Link
-          href="/summarizer"
+          href="/paraphraser"
           className="bg-azure-blue text-cotton-white px-4 py-3 rounded-md"
         >
-          Summarize
+          Paraphrase
         </Link>
-        <Link href="/paraphraser">Paraphrase</Link>
       </section>
 
+      {/* main content of the page */}
       <section></section>
     </main>
   );
--- app/summarizer/page.tsx
+++ app/summarizer/page.tsx
@@ -1,10 +1,26 @@
-import React from 'react'
+import React from "react";
+import Link from "next/link";
+import { NavBar } from "../components";
 
 const Summarizer = () => {
   return (
-    <div>
-      
-    </div>
+    <main>
+      <section className="p-7 md:px-10 lg:px-14 bg-azure-blue md:bg-cotton-white">
+        <NavBar />
+      </section>
+      <section className="flex items-center justify-center gap-5 py-10">
+        <Link
+          href="/summarizer"
+          className="bg-azure-blue text-cotton-white px-4 py-3 rounded-md"
+        >
+          Summarize
+        </Link>
+        <Link href="/paraphraser">Paraphrase</Link>
+      </section>
+
+      {/* main content of the page */}
+      <section></section>
+    </main>
   )
 }
 
