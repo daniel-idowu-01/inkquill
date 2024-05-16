646700a7205b03aa6fb41364997a217a89fae5e7
daniel-idowu-01 <danielidowu414@gmail.com>
2024-05-16 01:59:34 +0100

    allowed access to pdfs

--- app/summarizer/upload/page.tsx
+++ app/summarizer/upload/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import axios from "axios";
-import React, { useState, useEffect, ChangeEventHandler } from "react";
+import React from "react";
 import Link from "next/link";
 import Button from "@/app/ui/Button";
 import { NavBar } from "@/app/components";
@@ -10,22 +10,26 @@ import useOcrApi from "@/app/utils/useOcrApi";
 const Upload = () => {
   const preset_key = "yykpflgd";
   const cloud_name = "ds8bolg2f";
-  const { setFile, setFileUrl, getText } = useOcrApi();
+  const file_icon =
+    "https://imgs.search.brave.com/PkNC4u9TBqgKPrkKztC8BMORU8gNafaa0E1jKCgBEYw/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9jZG4u/aWNvbi1pY29ucy5j/b20vaWNvbnMyLzg4/Ni9QTkcvNTEyL2Zp/bGVfSW1hZ2VfZG93/bmxvYWRfaWNvbi1p/Y29ucy5jb21fNjg5/NDIucG5n";
+  const { fileUrl, setFileUrl, getText } = useOcrApi();
 
-  // update upload file
+  // upload file function
   const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const uploadedFile = event.target.files?.[0]; // Null check
-    const formdata = new FormData();
-    formdata.append("file", uploadedFile)
-    formdata.append("upload_preset", preset_key);
-    axios
-      .post(`https://api.cloudinary.com/v1_1/${cloud_name}/image/upload`, formdata)
-      .then((res) => setFileUrl(res.data.secure_url))
-      .catch((err) => console.log(err));
-    /* if (uploadedFile) {
-      setFile(uploadedFile);
-      setFileUrl(URL.createObjectURL(uploadedFile)); // Create blob URL
-    } */
+    // Check if uploadedFile is defined
+    if (uploadedFile) {
+      const formdata = new FormData();
+      formdata.append("file", uploadedFile);
+      formdata.append("upload_preset", preset_key);
+      axios
+        .post(
+          `https://api.cloudinary.com/v1_1/${cloud_name}/image/upload`,
+          formdata
+        )
+        .then((res) => setFileUrl(res.data.secure_url))
+        .catch((err) => console.log(err));
+    }
   };
 
   return (
@@ -46,9 +50,13 @@ const Upload = () => {
       {/* main content of the page */}
       <section className="mx-auto bg-cotton-white w-[90%] md:w-[70%] h-96 pt-10">
         <article className="w-1/2 lg:w-1/4 mx-auto flex flex-col justify-center gap-10 h-full">
+          {fileUrl && (
+            <img src={file_icon} alt="" className="w-20 h-20 mx-auto" />
+          )}
           <Input
             type="file"
             onChange={handleFileChange}
+            accept=".pdf"
             className=" border-azure-blue text-azure-blue"
           />
           <div onClick={getText} className="mx-auto">
--- app/utils/useOcrApi.tsx
+++ app/utils/useOcrApi.tsx
@@ -2,7 +2,6 @@ import { useState } from "react";
 
 const useOcrApi = () => {
   const apiKey = process.env.API_KEY ?? "helloworld";
-  const [file, setFile] = useState<File | null>(null);
   const [fileUrl, setFileUrl] = useState<string>("");
   const [isLoading, setIsLoading] = useState(false);
 
@@ -13,18 +12,14 @@ const useOcrApi = () => {
 
   // form info
   const myHeaders = new Headers();
-  myHeaders.append("apikey", apiKey);
+  /* myHeaders.append("apikey", apiKey); */
 
   const formdata = new FormData();
+  formdata.append("apikey", apiKey);
   formdata.append("language", "eng");
   formdata.append("isOverlayRequired", "false");
-  if (fileUrl !== null) {
-    formdata.append("url", fileUrl);
-    /* "https://imgs.search.brave.com/jL9Wn0n4qvKp6DdN3Z8gjm7SJ7op4NDzZY0DsDEsN-E/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9pLnBp/bmltZy5jb20vb3Jp/Z2luYWxzLzUxLzQz/LzM3LzUxNDMzN2Ux/ZWI0ZGU4ZTU1MmRk/MGNjYWVhODM3Zjhj/LmpwZw" */
-  } else {
-    console.error("File is null. Cannot append null value to FormData.");
-  }
-  formdata.append("filetype", "png");
+  formdata.append("url", fileUrl);
+  formdata.append("filetype", "pdf");
   formdata.append("iscreatesearchablepdf", "false");
   formdata.append("issearchablepdfhidetextlayer", "false");
 
@@ -48,7 +43,7 @@ const useOcrApi = () => {
         setIsLoading(false);
       });
   };
-  return { getText, setFile, setFileUrl };
+  return { getText, setFileUrl, fileUrl };
 };
 
 export default useOcrApi;
