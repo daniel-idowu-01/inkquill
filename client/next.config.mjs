35441b26ef1313a2c4e2a7ada0d267d09a5a63d9
daniel-idowu-01 <danielidowu414@gmail.com>
2024-05-19 00:11:38 +0100

    separate client amd server

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -4,7 +4,6 @@ const nextConfig = {
     OCR_API_KEY: "K83747338588957",
     CLOUDINARY_PRESET_KEY: "yykpflgd",
     CLOUDINARY_CLOUD_NAME: "ds8bolg2f",
-    COHERE_API_KEY: "EdQKzAUeWRUerp60wlrOLt3TjwprQgApokD1imw2",
   },
 };
 
